862b41ef428999106317a5ea1ecdf4e7be14ea30
OleoKeyo <oleokeyo@gmail.com>
2021-10-04 23:21:20 +0300

    fix some

--- Assets/Scripts/Cat/RayShoot.cs
+++ Assets/Scripts/Cat/RayShoot.cs
@@ -5,7 +5,10 @@ public class RayShoot : MonoBehaviour
 {
     public Rigidbody2D rigidbody2D;
     public float speed;
-    
+    public ElementType type;
+    private const string PlayerTag = "Player";
+    private const string DoorTag = "Door";
+
     public void ShootTowards(Vector3 from, Vector3 to)
     {
         Vector3 shootDir = (to - from).normalized;
@@ -15,5 +18,21 @@ public class RayShoot : MonoBehaviour
     public void Update()
     {
         rigidbody2D.velocity = transform.right * speed;
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.gameObject.name);
+        if (other.CompareTag(DoorTag))
+        {
+            var door = other.GetComponent<Door>();
+            door.CheckElementType(type);
+            Debug.Log("DoorHit");
+        }
+        
+        if (other.CompareTag(PlayerTag))
+        {
+            Debug.Log("PlayerHit");
+        }
     }
 }
