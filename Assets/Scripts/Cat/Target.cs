c0277d182a8bf958156246a208daf0bdf3de137e
OleoKeyo <oleokeyo@gmail.com>
2021-10-04 21:01:41 +0300

    new beam sprites

---
+++ Assets/Scripts/Cat/Target.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+  public SpriteRenderer spriteRenderer;
+  
+  private void Awake()
+  {
+    InstantFade();
+    StartCoroutine(FadeOut());
+  }
+
+  private void InstantFade()
+  {
+    Color oldColor = spriteRenderer.color;
+    Color newColor = oldColor;
+    newColor.a = 0f;
+    spriteRenderer.color = newColor;
+  }
+
+  private IEnumerator FadeIn()
+  {
+    while (spriteRenderer.color.a > 0)
+    {
+      Color oldColor = spriteRenderer.color;
+      Color newColor = oldColor;
+      newColor.a -= 0.05f;
+      spriteRenderer.color = newColor;
+      yield return new WaitForSeconds(0.05f);
+    }
+    Destroy(gameObject);
+  }
+  private IEnumerator FadeOut()
+  {
+    while (spriteRenderer.color.a < 1)
+    {
+      Color oldColor = spriteRenderer.color;
+      Color newColor = oldColor;
+      newColor.a += 0.05f;
+      spriteRenderer.color = newColor;
+      yield return new WaitForSeconds(0.05f);
+    }
+    StartCoroutine(FadeIn());
+  }
+}
