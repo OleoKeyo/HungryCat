a5e1211cf2853c05ddee305d552829ce076b7c8c
OleoKeyo <oleokeyo@gmail.com>
2021-10-04 22:44:47 +0300

    Add beam

---
+++ Assets/Scripts/Cat/RayShoot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class RayShoot : MonoBehaviour
+{
+    public Rigidbody2D rigidbody2D;
+    public float speed;
+    
+    public void ShootTowards(Vector3 from, Vector3 to)
+    {
+        Vector3 shootDir = (to - from).normalized;
+        transform.right = shootDir;
+    }
+
+    public void Update()
+    {
+        rigidbody2D.velocity = transform.right * speed;
+    }
+}
